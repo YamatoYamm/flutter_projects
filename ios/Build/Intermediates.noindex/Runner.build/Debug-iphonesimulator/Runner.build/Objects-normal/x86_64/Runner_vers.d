4cdc84607a00872ba7f8c8201ff021922cd1e56d
Catalin Presneanu <presneanucatalin@yahoo.com>
2022-04-06 01:31:49 +0300

    Initial commit

---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/catalinpresneanu/IdeaProjects/flutter_projects/ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import '01/currency_converter.dart' as app;
+
+void main() {
+  app.main();
+}
